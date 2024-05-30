c8d6c446500c0940a32382c3d77dbe68236937b2
lear23 <130910243+lear23@users.noreply.github.com>
2024-05-30 12:56:49 +0200

    Added SignalR.Client i server nu funkar chat

--- BlazorAppSilicon/BlazorAppSilicon.Client/Program.cs
+++ BlazorAppSilicon/BlazorAppSilicon.Client/Program.cs
@@ -8,4 +8,6 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
+
+
 await builder.Build().RunAsync();
